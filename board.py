c9bebc851dce87c6a370af1f94e08466c089671b
Amogh Chaubey <chaubeyamogh@gmail.com>
2022-02-12 18:06:24 -0500

    BITBOARD IS PRINTING AND ALL PIECES ARE THERE!!!

--- board.py
+++ board.py
@@ -28,28 +28,45 @@ class Board:
         self.black_knight_bitboard = self.create_empty_bitmap()
         self.black_queen_bitboard = self.create_empty_bitmap()
        
+        self.initialize_pieces()
+
+
         self.bitboards = np.vstack(
             (self.white_rook_bitboard,
             self.white_knight_bitboard,
             self.white_bishop_bitboard,
             self.white_queen_bitboard,
             self.white_king_bitboard,
+            self.white_pawn_bitboard,
 
             self.black_rook_bitboard,
             self.black_knight_bitboard,
             self.black_bishop_bitboard,
             self.black_queen_bitboard,
-            self.black_king_bitboard,)
+            self.black_king_bitboard,
+            self.black_pawn_bitboard)
         )
 
-        self.initialize_pieces()
-
+        
     def update_bitboard_state(self):
         result = np.zeros(64, "byte")
         for board in self.bitboards:
             result = np.bitwise_or(board, result, dtype = "byte")
         self.bitboards = result
         
+    def pretty_print_bitboard(self):
+        val = ''
+        for i, square in enumerate(self.bitboards):
+            if not i % 8:
+                val += '\n'
+            if square:
+                val += 'X'
+                continue
+            val += '-'
+        print(val)
+
+
+
 
 
     def create_empty_bitmap(self):
@@ -62,10 +79,10 @@ class Board:
         self.white_knight_bitboard[6]=1
         self.white_bishop_bitboard[2] = 1
         self.white_bishop_bitboard[5] = 1
-        self.white_queen_bitboard[4] = 1
+        self.white_queen_bitboard[3] = 1
         self.white_king_bitboard[4] = 1
 
-        for i in range(8, 15):
+        for i in range(8, 16):
             self.white_pawn_bitboard[i] = 1
 
         self.black_rook_bitboard[63] = 1
@@ -78,7 +95,7 @@ class Board:
         self.black_queen_bitboard[59] = 1
         self.black_king_bitboard[60] = 1
 
-        for i in range(8, 15):
+        for i in range(48, 56):
             self.black_pawn_bitboard[i] = 1
 
         
