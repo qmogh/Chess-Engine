f85c1928dec0162afe96177eda6016ebfd8dd39f
Amogh Chaubey <chaubeyamogh@gmail.com>
2022-02-13 16:19:23 -0500

    25% Done lol

--- game.py
+++ game.py
@@ -1,3 +1,42 @@
+from position import Position
+
+"""
+notes:
+    - Game as stack of Positions
+    - each move pushes a position onto the stack
+"""
+
+
 class Game:
     def __init__(self):
-        self.history = []
+        self.history = []  # Stack of Position objects
+        initial_position = Position(None)
+        self.history.append(initial_position)
+        self.half_move_number = 0
+
+    def add_position(self, position) -> bool:
+        self.history.append(position)
+        self.half_move_number += 1
+        return True
+
+    def get_current_position(self) -> Position:
+        try:
+            return self.history[-1]
+        except IndexError:
+            print("You've reached the initial position.")
+
+    def get_last_position(self) -> Position:
+        try:
+            return self.history[-2]
+        except IndexError:
+            print("You've reached the initial position.")
+
+    def get_move(self, move_number):
+        """ TODO: get move by move_number """
+        pass
+
+    def take_back_move(self) -> Position:
+        try:
+            return self.history.pop()
+        except IndexError:
+            print("You've reached the initial position.")
--- position.py
+++ position.py
@@ -1,60 +1,75 @@
-from board import Board as bb
+from board import Board
 from constants import Color, Piece
 
+
+def generate_fen():
+    """ TODO: Generate FEN for the current state """
+    return '-- TODO: generate FEN --'
+
+
+def is_legal_move(move):
+    """ TODO: quasi-legal move check """
+    return True
+
+
 class Position:
-   # the initial state
-    def __init__(self):
-        self.board = None
+    """Represents the internal state of a chess position"""
+
+    def __init__(self, board):
+        if board is None:
+            self.board = Board()
+        else:
+            self.board = board
+
         self.to_move = Color.WHITE
-        self.castle_rights = { Color.WHITE: True, Color.BLACK: True }
-        self.en_passant_target = None
+
+        self.castle_rights = {Color.WHITE: True, Color.BLACK: True}
+        self.en_passant_target = None  # target square
         self.halfmove_clock = 0
 
         self.piece_map = {}
+        self.set_initial_piece_locations()
 
-    def get_piece_location(self):
+    def get_piece_locations(self):
         pass
 
-    def set_initial_piece_locs(self):
-        for i in range(8, 16):
-            self.piece_map[i] = Piece.wP
+    def set_initial_piece_locations(self):
+        self.piece_map[Piece.wP] = set([i for i in range(8, 16)])
+        self.piece_map[Piece.wR] = {0, 7}
+        self.piece_map[Piece.wN] = {1, 6}
+        self.piece_map[Piece.wB] = {2, 5}
+        self.piece_map[Piece.wQ] = {3}
+        self.piece_map[Piece.wK] = {4}
 
-        self.piece_map[0] = Piece.wR
-        self.piece_map[7] = Piece.wR
-        self.piece_map[1] = Piece.wN
-        self.piece_map[6] = Piece.wN
-        self.piece_map[2] = Piece.wB
-        self.piece_map[5] = Piece.wB
-        self.piece_map[3] = Piece.wQ
-        self.piece_map[4] = Piece.wK
-
-        for i in range(48, 56):
-            self.piece_map[i] = Piece.wP
-
-        self.piece_map[56] = Piece.bR
-        self.piece_map[63] = Piece.bR
-        self.piece_map[57] = Piece.bN
-        self.piece_map[62] = Piece.bN
-        self.piece_map[58] = Piece.bB
-        self.piece_map[61] = Piece.bB
-        self.piece_map[59] = Piece.bQ
-        self.piece_map[60] = Piece.bK
-
-        for i in range(48, 56):
-            self.black_P_bb[i] = 1
-
-
-
+        self.piece_map[Piece.bP] = set([i for i in range(48, 56)])
+        self.piece_map[Piece.bR] = {56, 63}
+        self.piece_map[Piece.bN] = {57, 62}
+        self.piece_map[Piece.bB] = {58, 61}
+        self.piece_map[Piece.bQ] = {59}
+        self.piece_map[Piece.bK] = {60}
 
     def make_move(self, move):
-        self.to_move = not self.to_move
-        if not self.is_legal_move(move):
+        if not is_legal_move(move):
             print('Illegal move')
+            return
 
-            
-        self.update_bitboards(move)
+        # Empty piece existing spot
+        self.piece_map[move.piece].remove(move.from_sq)
 
-    def update_bitboards(self):
-        pass
+        # Put piece into new spot
+        self.piece_map[move.piece].add(move.to_sq)
 
+        # TODO: update
+        self.castle_rights = self.castle_rights
 
+        # TODO: update
+        self.en_passant_target = self.en_passant_target
+
+        self.halfmove_clock += 1
+
+        # Data flows from MakeMove -> Position -> BitBoard -> Search
+        self.board.update_position(self.piece_map)
+
+        self.to_move = not self.to_move
+
+        return generate_fen()
